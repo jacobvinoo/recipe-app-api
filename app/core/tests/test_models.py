213cf374fdb26bf058d3a8077ca816b515433c34
Vinoo Jacob <jacobvinoo@gmail.com>
2019-04-18 17:02:27 +1200

    Added Recipe Model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -63,3 +63,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string represntation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Stake and Mushrom Sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
